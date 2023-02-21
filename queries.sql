7379832bee1a4fc455542c553027bb1603bb7217
shinhyo26okazaki <shibo6937@gmail.com>
2023-02-21 07:53:47 -0400

    Fix date problem

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
